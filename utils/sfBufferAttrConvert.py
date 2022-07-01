e5bec8e3993ebdfae336bcd1e40cfc32dd4c2293
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-07-01 17:00:12 +0000

    Add helper script for converting SfBufferAttr flags

--- utils/sfBufferAttrConvert.py
+++ utils/sfBufferAttrConvert.py
@@ -1,7 +1,6 @@
 from enum import IntFlag
 import argparse
 
-# Source: https://github.com/switchbrew/libnx/blob/62755bebe0ef0e90ae4918a96fe5334da64bec94/nx/include/switch/sf/service.h#L20-L29
 class SfBufferAttrs(IntFlag):
     SfBufferAttr_In                             = 0
     SfBufferAttr_Out                            = 1
@@ -18,20 +17,12 @@ class SfBufferAttrs(IntFlag):
 
 
 def hextype(string) -> int:
-    if string.startswith("0x"):
-        return int(string, 16)
-    else:
-        return int(string)
+    return int(string, 16)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("type_num", type=hextype, nargs="+", help="hex value(s) that should be converted")
 args = parser.parse_args()
 
-first_num = True
 for type_num in args.type_num:
-    if not first_num:
-        print()
-    else:
-        first_num = False
     print(f"{hex(type_num)} -> {f'{type_num:0>8b}'}")
-    print(f"{{{' | '.join([x.name for x in SfBufferAttrs.find_attrs(type_num)])}}}")
+    print(f"{{{' | '.join([x.name for x in SfBufferAttrs.find_attrs(type_num)])}}}\n")
