3e8a20af18ef688466fae7b9171fc96878b22230
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-07-07 11:51:09 +0000

    Add sizes to binary data logging

--- include/logging.hpp
+++ include/logging.hpp
@@ -12,6 +12,6 @@ namespace ams::log
     void DebugDataDump(const void *data, size_t size, const char *fmt, ...);
 
 #define DEBUG_LOG(fmt, ...) DebugLog(fmt "\n", ##__VA_ARGS__)
-#define DEBUG_DATA_DUMP(data, size, fmt, ...) DebugDataDump(data, size, fmt "\n", ##__VA_ARGS__)
+#define DEBUG_DATA_DUMP(data, size, fmt, ...) DebugDataDump(data, size, fmt "[%#x]:\n", ##__VA_ARGS__, size)
 
 }
